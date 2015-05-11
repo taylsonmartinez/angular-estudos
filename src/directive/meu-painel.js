f07bdc2690fefc81ef5c677deaeec07a1cf0a5d2
taylsonmartinez <taylson.martinez@gmail.com>
2015-05-11 14:24:37 -0300

    directive meu-painel

---
+++ src/directive/meu-painel.js
@@ -0,0 +1,12 @@
+angular.module('minhasDiretivas', [])
+    .directive('meuPainel', function() {
+        var directive = {
+                     restrict: 'E', 
+                     templateUrl: 'directive/meu-painel.html', 
+                     transclude: true, 
+                     scope: {
+                           titulo: '@'
+                     }
+                };
+        return directive;
+    });
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-var app = angular.module('Funcionarios',['ngRoute', 'ngResource', 'services']);
+var app = angular.module('Funcionarios',['ngRoute', 'ngResource', 'services', 'minhasDiretivas']);
 
 app.config(function($routeProvider){
     
